2d2285e04db44c794bf9f8cb6446c667bc9fafb4
FMY <n.fmy.0526@icloud.com>
2022-12-19 23:32:45 +0900

    社員のデータ構造を定義

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Member } from '../member';
 
 @Component({
   selector: 'app-members',
   templateUrl: './members.component.html',
-  styleUrls: ['./members.component.css']
+  styleUrls: ['./members.component.css'],
 })
 export class MembersComponent implements OnInit {
-
-  constructor() { }
+  member: Member = {
+    id: 1,
+    name: '田中太郎',
+  };
+  constructor() {}
   // componentが初期化されるときに実行されるメソッド
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
