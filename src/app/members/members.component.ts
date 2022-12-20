bf3b620a17d042bd6461830bb28387b0a19f9644
中村　郁弥 <120069589+n-fumiya@users.noreply.github.com>
2022-12-20 13:16:54 +0900

    ngFor員を覧表示,クリックイベントを追加し,社員を選択できるよう実装

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { Member } from '../member';
-
+import { MEMBERS } from '../mock-members';
 @Component({
   selector: 'app-members',
   templateUrl: './members.component.html',
   styleUrls: ['./members.component.css'],
 })
 export class MembersComponent implements OnInit {
+
+  members = MEMBERS;
+  selectedMember: Member;
+
   member: Member = {
     id: 1,
     name: '田中太郎',
@@ -14,4 +18,8 @@ export class MembersComponent implements OnInit {
   constructor() {}
   // componentが初期化されるときに実行されるメソッド
   ngOnInit(): void {}
+  // クリックイベント
+  onSelect(member: Member): void {
+    this.selectedMember = member;
+  }
 }
